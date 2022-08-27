a508fcadd0a66d05a9fa027e2b79938845350035
Wisula <99121477+Wisula@users.noreply.github.com>
2022-08-27 22:18:55 +0530

    Update Logo2.py

--- Telegram/modules/Logo2.py
+++ Telegram/modules/Logo2.py
@@ -104,7 +104,7 @@ async def logo_gen(event):
     )
     x = (image_width - w) / 2
     y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
+    draw.text((x, y), name, font=font, fill="white")
               stroke_width=strke, stroke_fill="black")
     flnme = f"logo.png"
     img.save(flnme, "png")
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [@SUPERIOR_BOTS](https://t.me/SUPERIOR_BOTS)",
+            caption="Logo by [@EpicbotsSl](https://t.me/EpicBotsSl)",
             force_document=False,
         )
         os.remove(flnme)
