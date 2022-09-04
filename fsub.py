3c6f6169ad6e609489118180a93912fcf31fcc32
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-09-04 21:37:45 +0530

    Update fsub.py

--- fsub.py
+++ fsub.py
@@ -1,26 +1,61 @@
-from pyrogram.types import *
-from pyrogram.errors import *
-from config import *
+import os
+import asyncio
+from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+uchannel = "CGSUpdates"
+
+async def ForceSub(bot: Client, event: Message):
+    try:
+        invite_link = await bot.create_chat_invite_link(chat_id=(int(uchannel) if uchannel.startswith("-100") else uchannel))
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, event)
+        return fix_
+    except Exception as err:
+        print(f"Unable to do Force Subscribe to {uchannel}\n\nError: {err}\n\nContact Support Group: https://t.me/CGSsupport")
+        return 200
+    try:
+        user = await bot.get_chat_member(chat_id=(int(uchannel) if uchannel.startswith("-100") else uchannel), user_id=event.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=event.from_user.id,
+                text="Sorry Dear, You are Banned to use me ☹️\nFeel free to say in our [Support Group](https://t.me/CGSsupport).",
+                parse_mode="markdown",
+                disable_web_page_preview=True,
+                reply_to_message_id=event.message_id
+            )
+            return 400
+        else:
+            return 200
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=event.chat.id,
+            text=""" 
+            ** {} Access declined** 📛
+
+Please 🪴[Join update channel](https://t.me/CGSUPDATES) to use commands ☘ Then try again.🍃. 
 
 
-async def forcesub(bot, update):
-        try:
-            await bot.get_chat_member(AUTH_CHANNEL, update.from_user.id)
-        except UserNotParticipant:
-            file_id = "CAACAgUAAxkBAAEFwKpjFHLuKbwggVDodnliDlNEMPCCwAACGQcAAtzhOFaSum5URZebkCkE"
-            await bot.send_sticker(update.from_user.id, file_id)
-            text = FORCESUB_TEXT
-            reply_markup = FORCESUB_BUTTONS
-            return await bot.send_message(update.from_user.id,
-            text=text,
-            reply_markup=reply_markup,
-            disable_web_page_preview=True)
-
-FORCESUB_TEXT = """
-        ❌Aƈƈҽʂʂ Dҽɳιҽԃ❌ 
-♻️Please join our channel & 𝐓𝐫𝐲 𝐚𝐠𝐚𝐢𝐧♻️
-"""
-
-FORCESUB_BUTTONS = InlineKeyboardMarkup([[
-                InlineKeyboardButton('🔰 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 🔰', url='https://t.me/EpicBotsSl')
-            ]])
+**@CGSUPDATES**🌺
+            """.format(event.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Join Our Updates Channel 🗣", url=invite_link.invite_link)
+                    ],
+                ]
+            ),
+            disable_web_page_preview=True,
+            reply_to_message_id=event.message_id
+        )
+        return 400
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, event)
+        return fix_
+    except Exception as err:
+        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/CGSsupport")
+        return 200
+    
